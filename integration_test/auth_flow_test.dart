5167ea755de305b8644b4c246c343ba7b04d0b30
Viet T. Nguyen <thanhviet.1081@gmail.com>
2022-08-29 12:54:07 +0700

    Add auth integration test

---
+++ integration_test/auth_flow_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import '../test/src/robot.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('Sign in and sign out flow', (tester) async {
+    final r = Robot(tester: tester);
+    await r.pumpMyapp();
+    r.expectFindAllProductCards();
+    await r.openPopupMenu();
+    await r.authRobot.openEmailAndPasswordSignInScreen();
+    await r.authRobot.signInWithEmailAndPassword();
+    r.expectFindAllProductCards();
+    await r.openPopupMenu();
+    await r.authRobot.openAccountScreen();
+    await r.authRobot.tapLogoutButton();
+    await r.authRobot.tapDialogLogoutButton();
+    r.expectFindAllProductCards();
+  });
+}
